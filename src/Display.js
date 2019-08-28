ad28f6cec0f3dcffcf69e49e1554b579add357bc
Chris Underwood <cunderwoodmn@gmail.com>
2019-08-27 19:39:59 -0700

    grabbed more data and styled the main page layout

--- src/Display.js
+++ src/Display.js
@@ -1,8 +1,32 @@
 import React from 'react';
 
 const display = (props) => {
-return (<div>Can you the display component?
-  <h1>The weather in {props.name} is:</h1>
+  console.log("display props", props)
+  const farenTemp = (((props.state.temp) - 273.15) * 9/5 + 32 ).toFixed(2);
+  let desc = ''
+  let lon = 0;
+  let lat = 0;
+  if (props.weather) {
+   desc = props.weather['main'];
+  }
+
+  if (props.state.lon){
+    lon = props.state.lon
+  }
+  if (props.state.lat){
+    lat = props.state.lat
+  }
+
+
+return (<div className={'weatherBox'}>
+  <h3>The current weather in {props.state.name} is:</h3>
+  <ul>
+    <li><strong>Tempurature:</strong> {farenTemp} Farenheit</li>
+    <li><strong>Atmospheric Pressure:</strong> {props.state.pressure}</li>
+    <li><strong>Humidity:</strong> {props.state.humidity}%</li>
+    <li><strong>Sky:</strong> {desc}</li>
+    
+  </ul>
 </div>)
 }
 
--- src/Fetching.js
+++ src/Fetching.js
@@ -8,15 +8,37 @@ class Fetching extends Component {
   constructor(props) {
     super(props)
     this.state={lat: null, lon: null,
-    name: '', }
+    name: '', temp: null,
+    pressure: null,
+    humidity: null,
+    weather: [],
+    lat: null,
+    lon: null,
+  }
+
   }
 
   async componentDidMount(){
     try {                            
-    const {data}  = await axios.get(process.env.REACT_APP_API_URL + 'lat=34.2104&lon=-77.915543&APPID=' + process.env.REACT_APP_API_KEY);
+    const {data, }  = await axios.get(process.env.REACT_APP_API_URL + 'lat=34.2104&lon=-77.915543&APPID=' + process.env.REACT_APP_API_KEY);
     console.log("api_results...", data)
-      const name = data.name
-      this.setState({name: name})
+    console.log("api_results_weather...", data.weather)
+      const nameVar = data.name
+      const tempVar = data.main.temp_max
+      const pressureVar = data.main.pressure
+      const humidityVar = data.main.humidity
+      const weatherVar = data.weather
+      const latVar = data.coord['lat']
+      const lonVar = data.coord['lon']
+ 
+      this.setState({name: nameVar, 
+        temp: tempVar, 
+        humidity: humidityVar, 
+        pressure: pressureVar, 
+        weather: weatherVar,
+        lat: latVar,
+        lon: lonVar,
+      })
 
     } catch (error) {
       console.log(error)
@@ -25,7 +47,13 @@ class Fetching extends Component {
 
 
   render(){
-    return(<div><h1>Weather App</h1><Display name={this.state.name}/></div>)
+    return(<div >
+      <h1 className="mainHeader">Weather App</h1>
+      <Display 
+      state={this.state}
+      weather={this.state.weather[0]}
+      />
+   </div>)
   }
 }
 export default Fetching;
