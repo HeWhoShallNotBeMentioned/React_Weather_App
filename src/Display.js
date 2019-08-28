b7ee33c5d69ec31fd6688678925dd44ff12f4d17
Chris Underwood <cunderwoodmn@gmail.com>
2019-08-27 17:12:26 -0700

    retrieving data from the api and able to display basic data to the screen

---
+++ src/Display.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const display = (props) => {
+return (<div>Can you the display component?
+  <h1>The weather in {props.name} is:</h1>
+</div>)
+}
+
+export default display;
--- src/Fetching.js
+++ src/Fetching.js
@@ -1,13 +1,31 @@
 import React, {Component} from 'react';
+
+import axios from 'axios';
+import Display from './Display';
 require('dotenv').config();
 
-
 class Fetching extends Component {
+  constructor(props) {
+    super(props)
+    this.state={lat: null, lon: null,
+    name: '', }
+  }
+
+  async componentDidMount(){
+    try {                            
+    const {data}  = await axios.get(process.env.REACT_APP_API_URL + 'lat=34.2104&lon=-77.915543&APPID=' + process.env.REACT_APP_API_KEY);
+    console.log("api_results...", data)
+      const name = data.name
+      this.setState({name: name})
+
+    } catch (error) {
+      console.log(error)
+    }
+  }
 
 
   render(){
-    return(<div><p>Can you see me?</p><a href={process.env.REACT_APP_API_URL + '&APPID=' + process.env.REACT_APP_API_KEY}>Get weather</a></div>)
+    return(<div><h1>Weather App</h1><Display name={this.state.name}/></div>)
   }
 }
-
 export default Fetching;
